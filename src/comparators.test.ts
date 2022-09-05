90ba02e3febe206f7ea09b36e6c2c9f081f655dd
Kevin Maes <kevin@kevinmaes.com>
2022-09-05 07:07:04 -0400

    Work on nested comparison. WIP

--- src/comparators.test.ts
+++ src/comparators.test.ts
@@ -4,7 +4,20 @@ describe('comparators', () => {
   it('should compare objects by name value', () => {
     const array = [{ name: 'bravo' }, { name: 'alpha' }];
 
-    const result = array.sort(byPropAtoZ('name'));
+    const subject = byPropAtoZ('name');
+    const result = array.sort(subject);
+    // const result = array.sort(byPropAtoZ('name'));
+
+    expect(result).toEqual([{ name: 'alpha' }, { name: 'bravo' }]);
+  });
+
+  it.skip('should compare objects by nested name value', () => {
+    const array = [
+      { nested: { name: 'bravo' } },
+      { nested: { name: 'alpha' } },
+    ];
+
+    const result = array.sort(byPropAtoZ('nested.name'));
 
     expect(result).toEqual([{ name: 'alpha' }, { name: 'bravo' }]);
   });
--- src/comparators.ts
+++ src/comparators.ts
@@ -1,18 +1,75 @@
+type NestedObj<TObj> = Record<string, TObj[keyof TObj] | Record<string, TObj>>;
+type NestedValue<TObj> = NestedObj<TObj>;
+
 type Comparator<TElement extends string | number | object> = (
   a: TElement,
   b: TElement
 ) => -1 | 0 | 1;
 
 type PropComparatorFactory = <TObj extends object>(
-  propName: keyof TObj
+  path: keyof TObj
 ) => Comparator<TObj>;
 
-export const byPropAtoZ: PropComparatorFactory = (propName) => (a, b) => {
-  if (a[propName] < b[propName]) {
-    return -1;
-  }
-  if (a[propName] > b[propName]) {
-    return 1;
-  }
-  return 0;
-};
+const _byPropAtoZ =
+  <TObj extends object>(path: keyof TObj): Comparator<TObj> =>
+  (a, b) => {
+    if (a[path] < b[path]) {
+      return -1;
+    }
+    if (a[path] > b[path]) {
+      return 1;
+    }
+    return 0;
+  };
+
+// const byPropAtoZ = <TObj extends object>(
+//   path: keyof NestedObj<TObj>
+// ): Comparator<TObj> => {
+//   const [currentPath, ...remainingPath] = path.split('.');
+//   let comparator;
+
+//   if (remainingPath.length === 0) {
+//     comparator = _byPropAtoZ(currentPath);
+//   } else {
+//     const nextLevelAObj = a[currentPath as keyof TObj];
+//     const nextLevelBObj = b[currentPath as keyof TObj];
+//     comparator = byPropAtoZ(remainingPath.join('.'));
+//     // predicate = byPropAtoZ(remainingPath.join('.')).bind(
+//     //   null,
+//     //   nextLevelAObj,
+//     //   nextLevelBObj
+//     // );
+//   }
+
+//   return (a, b) => {
+//     // let predicate;
+//     // if (remainingPath.length === 0) {
+//     //   predicate = _byPropAtoZ(currentPath).bind(null, a, b);
+//     // } else {
+//     //   const nextLevelAObj = a[currentPath as keyof TObj];
+//     //   const nextLevelBObj = b[currentPath as keyof TObj];
+//     //   predicate = byPropAtoZ(remainingPath.join('.')).bind(
+//     //     null,
+//     //     nextLevelAObj,
+//     //     nextLevelBObj
+//     //   );
+//     // }
+//     // return predicate();
+//   };
+// };
+
+const byPropAtoZ =
+  <TObj extends object>(path: keyof NestedObj<TObj>) =>
+  (a: any, b: any) => {
+    const [currentPath, ...remainingPath] = path.split('.');
+
+    if (remainingPath.length === 0) {
+      const predicate = _byPropAtoZ(currentPath);
+      return predicate(a, b);
+    } else {
+      const nextLevelAObj = a[currentPath as keyof TObj];
+      const nextLevelBObj = b[currentPath as keyof TObj];
+      const predicate = byPropAtoZ(remainingPath.join('.'));
+      return predicate(nextLevelAObj, nextLevelBObj);
+    }
+  };
