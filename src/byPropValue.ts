10514cfd17ecd36a77e66db831b7055cf3470ab2
Kevin Maes <kevin@kevinmaes.com>
2022-09-05 21:24:42 -0400

    Add private _get to retrieve nested values (#3)
    * Add private _get and refactor check against nested values
    
    * Separate _get into own _internal folder

--- src/byPropValue.ts
+++ src/byPropValue.ts
@@ -1,24 +1,24 @@
+import { NestedObj, NestedValue } from './types/types';
+import { _get } from './_internal/_get';
+
 interface Options {
   caseInsensitive?: boolean;
   matchUndefined?: boolean;
 }
 
-type NestedObj<TObj> = Record<string, TObj[keyof TObj] | Record<string, TObj>>;
-type NestedValue<TObj> = NestedObj<TObj>;
-
 /**
  *
  * @param propName String name of the object property to look up.
- * @param value A value to match against obj[propName].
- *  typeof obj[propName] and typeof value must match.
+ * @param targetValue A targetValue to match against obj[propName].
+ *  typeof obj[propName] and typeof targetValue must match.
  * @param insensitive For strings, whether matching is case-insensitive, default is false.
  * @returns A function that takes an object argument and returns a boolean as to if a match was found.
  */
-const _byPropValue =
-  <TObj>(
-    propName: keyof TObj,
-    value: TObj[keyof TObj],
-    { caseInsensitive, matchUndefined }: Options = {
+export const byPropValue =
+  <TObj extends Record<string, any>>(
+    path: keyof NestedObj<TObj>,
+    targetValue: NestedValue<TObj> | any,
+    { matchUndefined, caseInsensitive }: Options = {
       caseInsensitive: false,
       matchUndefined: false,
     }
@@ -26,61 +26,22 @@ const _byPropValue =
   (obj: TObj): boolean => {
     if (typeof obj === 'undefined') return false;
 
-    const propValue = obj[propName];
+    const propValue = _get(path, obj);
     if (typeof propValue === 'undefined') {
       if (matchUndefined) {
-        return typeof value === 'undefined';
+        return typeof targetValue === 'undefined';
       }
       return false;
     }
 
-    if (typeof propValue !== typeof value) return false;
+    if (typeof propValue !== typeof targetValue) return false;
 
     const valuesAreStrings =
-      typeof propValue === 'string' && typeof value === 'string';
+      typeof propValue === 'string' && typeof targetValue === 'string';
 
     if (valuesAreStrings && caseInsensitive) {
-      return propValue.toLocaleLowerCase() === value.toLowerCase();
+      return propValue.toLocaleLowerCase() === targetValue.toLowerCase();
     }
 
-    return propValue === value;
-  };
-
-/**
- *
- * @param propName String name of the object property to look up.
- * @param value A value to match against obj[propName].
- *  typeof obj[propName] and typeof value must match.
- * @param insensitive For strings, whether matching is case-insensitive, default is false.
- * @returns A function that takes an object argument and returns a boolean as to if a match was found.
- */
-export const byPropValue =
-  <TObj extends Record<string, any>>(
-    path: keyof NestedObj<TObj>,
-    value: NestedValue<TObj> | any,
-    options: Options = {
-      caseInsensitive: false,
-      matchUndefined: false,
-    }
-  ) =>
-  (obj: TObj): boolean => {
-    const [currentPath, ...remainingPath] = path.split('.');
-    let predicate;
-
-    if (remainingPath.length === 0) {
-      predicate = _byPropValue(
-        path as keyof TObj,
-        value as TObj[keyof TObj],
-        options
-      ).bind(null, obj);
-    } else {
-      const nextLevelObj = obj[currentPath as keyof TObj];
-      predicate = byPropValue(
-        remainingPath.join('.'),
-        value as typeof nextLevelObj[keyof typeof nextLevelObj],
-        options
-      ).bind(null, nextLevelObj);
-    }
-
-    return predicate();
+    return propValue === targetValue;
   };
