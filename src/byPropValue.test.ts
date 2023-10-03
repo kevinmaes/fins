a35f72978e596d7f6555b9d99a2eefacff78c1dc
Kevin Maes <kevin@kevinmaes.com>
2023-10-03 23:48:26 +0200

    Refactor tests

--- src/byPropValue.test.ts
+++ src/byPropValue.test.ts
@@ -11,78 +11,71 @@ describe('byPropValue', () => {
     });
 
     it('should return true for a matching number prop value', () => {
-      const obj = {
-        prop: 0,
-      };
-
       const subject = byPropValue('prop', 0);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 0,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false when not matching a number prop value', () => {
-      const obj = {
-        prop: 0,
-      };
-
       const subject = byPropValue('prop', 1);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 0,
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return true for a matching boolean prop value of true', () => {
-      const obj = {
-        prop: true,
-      };
-
       const subject = byPropValue('prop', true);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: true,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return true for a matching boolean prop value of false', () => {
-      const obj = {
-        prop: false,
-      };
-
       const subject = byPropValue('prop', false);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: false,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return true for a matching string prop value', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'value');
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false for no matching string prop value', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'otherValue');
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return false if the prop value and value are of different types', () => {
-      const obj = {
-        prop: 0,
-      };
-
       const subject = byPropValue('prop', '0');
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 0,
+      });
 
       expect(result).toBe(false);
     });
@@ -91,51 +84,44 @@ describe('byPropValue', () => {
   describe('matching non-primitive value types', () => {
     it('should return true for a matching object value', () => {
       const valueObj = {};
-
-      const obj = {
-        prop: valueObj,
-      };
-
       const subject = byPropValue('prop', valueObj);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: valueObj,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false for a non-matching object value', () => {
       const valueObj = {};
-
-      const obj = {
-        prop: valueObj,
-      };
-
       const subject = byPropValue('prop', {});
-      const result = subject(obj);
+
+      const result = subject({
+        prop: valueObj,
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return true for a matching array value', () => {
       const valueArray = [1, 2];
-
-      const obj = {
-        prop: valueArray,
-      };
-
       const subject = byPropValue('prop', valueArray);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: valueArray,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false for a non-matching array value', () => {
       const valueArray = [1, 2];
-      const obj = {
-        prop: valueArray,
-      };
-
       const subject = byPropValue('prop', []);
-      const result = subject(obj);
+
+      const result = subject({
+        prop: valueArray,
+      });
 
       expect(result).toBe(false);
     });
@@ -143,87 +129,79 @@ describe('byPropValue', () => {
 
   describe('matching with options arg', () => {
     it('should return true for a matching string prop value of different case when caseInsensitive option is true', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'VALUE', {
         caseInsensitive: true,
       });
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false for a non-matching string prop value when caseInsensitive option is true', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'otherValue', {
         caseInsensitive: true,
       });
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return false for a similar string prop of different case', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'VALUE');
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return false for no matching string prop value when caseInsensitive option is false', () => {
-      const obj = {
-        prop: 'value',
-      };
-
       const subject = byPropValue('prop', 'VALUE', {
         caseInsensitive: false,
       });
-      const result = subject(obj);
+
+      const result = subject({
+        prop: 'value',
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return false if an object prop value is undefined but the value passed is not undefined', () => {
-      const obj0 = {
-        propA: undefined,
-      };
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const subject = byPropValue('propA', 'not-undefined');
-      const result = subject(obj0);
+
+      const result = subject({
+        propA: undefined,
+      });
 
       expect(result).toBe(false);
     });
 
     it('should return true if an object prop value and value are undefined and the matchUndefined option is true', () => {
-      const obj0 = {
-        propA: undefined,
-      };
-
       const subject = byPropValue('propA', undefined, { matchUndefined: true });
-      const result = subject(obj0);
+
+      const result = subject({
+        propA: undefined,
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return false if an object prop value and value are undefined and the matchUndefined option is false', () => {
-      const obj0 = {
-        propA: undefined,
-      };
-
       const subject = byPropValue('propA', undefined, {
         matchUndefined: false,
       });
-      const result = subject(obj0);
+
+      const result = subject({
+        propA: undefined,
+      });
 
       expect(result).toBe(false);
     });
@@ -231,29 +209,27 @@ describe('byPropValue', () => {
 
   describe('nested level values', () => {
     it('should return true for a second level matching prop value', () => {
-      const obj = {
+      const subject = byPropValue('first.second', 'value');
+
+      const result = subject({
         first: {
           second: 'value',
         },
-      };
-
-      const subject = byPropValue('first.second', 'value');
-      const result = subject(obj);
+      });
 
       expect(result).toBe(true);
     });
 
     it('should return true for a third level matching prop value', () => {
-      const obj = {
+      const subject = byPropValue('first.second.third', 'value');
+
+      const result = subject({
         first: {
           second: {
             third: 'value',
           },
         },
-      };
-
-      const subject = byPropValue('first.second.third', 'value');
-      const result = subject(obj);
+      });
 
       expect(result).toBe(true);
     });
@@ -265,7 +241,7 @@ describe('byPropValue', () => {
 
       const result = array.find(byPropValue('prop', 'b'));
 
-      expect(result).toEqual({ prop: 'b' });
+      expect(result).toBe(array[1]);
     });
 
     it('should find an array element by matching nested prop value', () => {
@@ -277,7 +253,7 @@ describe('byPropValue', () => {
 
       const result = array.find(byPropValue('first.second', 'b'));
 
-      expect(result).toEqual({ first: { second: 'b' } });
+      expect(result).toBe(array[1]);
     });
   });
 });
