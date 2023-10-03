a9a5bdac9c5e54753cdcbdb661fcd303d8144c61
Kevin Maes <kevin@kevinmaes.com>
2023-10-03 23:57:22 +0200

    Add more methods to barrel export file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { filterMap } from './filterMap';
+export { get } from './get';
+export { byValue } from './byValue';
 export { byPropValue } from './byPropValue';
+export { filterMap } from './filterMap';
 export { byPropAtoZ } from './comparators';
 export { insertIf } from './insertIf';
